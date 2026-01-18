ac00fdccec745dadd47243cc261a7a036772e10a
realdanvanth <realdanvanth@gmail.com>
2026-01-18 12:35:02 +0530

    Deploy to /calculator subdirectory

--- Calculator/vite.config.js
+++ Calculator/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/23CSE461/',   // ⚠️ repo name
+  base: '/23CSE461/calculator/',   // ⚠️ repo name + subfolder
 })
 
